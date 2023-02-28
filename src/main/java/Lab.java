d23df5d0e588cf75c784443141eb1025ea177b5d
Luis Andrade <santiago16999@gmail.com>
2023-02-28 06:29:52 +0000

    if statement passcheck

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,12 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        int value = 0;
+        if(bool == true){
+            value = x;
+        }else if(bool == false){
+            value = y;
+        }
+        return value;
     }
 }
